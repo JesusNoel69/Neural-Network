8100b786997218ed0a87eee5acc8f8d10ec92eb9
Jesus <jnoel883@gmail.com>
2024-08-06 19:01:12 -0600

    Conflictos con clase perceptron.

--- Neural Network in C#/Layer.cs
+++
@@ -1,8 +0,0 @@
-namespace Neural_Network_in_C_
-{
-    public class Layer
-    {
-        public List<Perceptron> layer;
-
-    }
-}
--- Neural Network in C#/Perceptron.cs
+++ Neural Network in C#/Perceptron.cs
@@ -1,18 +1,26 @@
-using System.Runtime.InteropServices;
-
 namespace Neural_Network_in_C_
 {
         public class Perceptron
     {
-        private float[] inputs;
-        private float[] weights=[];
-        private float bias;
-        private float learningRate;
-        private string typeActivationFunction;
-        public float output;
-        public int espectedOutput;
-        private float error = 0.0f;
-        public static float[] RandomizeNumber(int length){
+        public float[] inputs { get; set; }
+        public float[] weights { get; set; }
+        public float bias { get; set; }
+        public float learningRate { get; set; }
+        public string typeActivationFunction { get; set; }
+        public float output { get; set; }
+        public float espectedOutput { get; set; }
+        public float error = 0.0f;
+
+        public Perceptron(int inputLength, string typeActivationFunction = "step", float learningRate = 0.1F)
+        {
+            weights = RandomizeNumber(inputLength);
+            bias = RandomizeNumber(1)[0];
+            this.learningRate = learningRate;
+            this.typeActivationFunction = typeActivationFunction;
+        }
+
+        public static float[] RandomizeNumber(int length)
+        {
             Random random = new();
             float[] randomNumbers = new float[length];
             for (int eachInput = 0; eachInput < length; eachInput++)
@@ -22,57 +30,27 @@ namespace Neural_Network_in_C_
             }
             return randomNumbers;
         }
-        public Perceptron(int inputLength, string typeActivationFunction="step", float learningRate=0.1F)
-        {
-            this.learningRate = learningRate;
-            bias = RandomizeNumber(1)[0];
-            weights = RandomizeNumber(inputLength);
-            this.typeActivationFunction = typeActivationFunction;
 
-        }
-
-        public void Train(float[][] trainingInputs, int[] trainingOutputs, int epochs)
+        public void Show()
         {
-            for (int epoch = 0; epoch < epochs; epoch++)
+            Console.Write("Error: " + error + " ");
+            Console.WriteLine("Sesgo: " + bias);
+            Console.Write("salida: " + output + " ");
+            Console.WriteLine("salida esperada: " + espectedOutput);
+            foreach (var weight in weights)
             {
-                bool accept = true;
-                System.Console.WriteLine("-------------");
-                Console.WriteLine("iteracion: "+(epoch+1));
-                for (int eachInput = 0; eachInput < trainingInputs.Length; eachInput++)
-                {
-                    inputs = trainingInputs[eachInput]; //[0,0]
-                    espectedOutput = trainingOutputs[eachInput];//0
-                    output = ActivationFunction(typeActivationFunction);
-                    error =  espectedOutput-output;
-                    if (error != 0)
-                    {
-                        accept = false;
-                        UpdateWeights(inputs.Length);
-                    }
-                    Show();
-                }
-                System.Console.WriteLine("-------------");
-                if (accept)
-                {
-                    break;
-                }
-            }
-        }
-        public void Show(){
-            Console.Write("Error: "+error+" ");
-            Console.WriteLine("Sesgo: "+bias);
-            Console.Write("salida: "+output+" ");
-            Console.WriteLine("salida esperada: "+espectedOutput);
-            foreach(var weight in weights){
-                Console.Write("pesos: "+weight+" ");
+                Console.Write("pesos: " + weight + " ");
             }
             Console.WriteLine("");
         }
-        public void UpdateWeights(int length){
-            for(int eachInput=0; eachInput<length; eachInput++){
-                weights[eachInput]+=learningRate*error*inputs[eachInput];
+
+        public void UpdateWeights(int length)
+        {
+            for (int eachInput = 0; eachInput < length; eachInput++)
+            {
+                weights[eachInput] += learningRate * error * ActivationFunctionDerivative(output) * inputs[eachInput];
             }
-            bias+=learningRate*error;
+            bias += learningRate * error * ActivationFunctionDerivative(output);
         }
 
         public float WeightedSum()
@@ -97,20 +75,36 @@ namespace Neural_Network_in_C_
                 _ => StepFunction(),
             };
         }
-        //1 o 0
-        public int StepFunction()=> WeightedSum()>=0? 1 : 0;
-        //mapea el valor entre 0 y 1
-        public float SigmoidFunction(){
+
+        public float ActivationFunctionDerivative(float x)
+        {
+            return typeActivationFunction switch
+            {
+                "sigmoid" => SigmoidDerivative(x),
+                "tanh" => TanhDerivative(x),
+                "relu" => ReLUDerivative(x),
+                _ => StepDerivative(),
+            };
+        }
+
+        public int StepFunction() => WeightedSum() >= 0 ? 1 : 0;
+        public float StepDerivative() => 1; //parametro x
+        public float SigmoidFunction()
+        {
             float x = WeightedSum();
             return (float)(1 / (1 + Math.Exp(-x)));
         }
-        //mapea el valor entre -1 y 1 
-        public float Thanh(){
+        public float SigmoidDerivative(float x) => x * (1 - x);
+
+        public float Tanh()
+        {
             float x = WeightedSum();
             return (float)((Math.Exp(x) - Math.Exp(-x)) / (Math.Exp(x) + Math.Exp(-x)));
         }
-        //positivo devuelve el valor sino devuelve 0
-        public float ReLU(){ 
+        public float TanhDerivative(float x) => 1 - x * x;
+
+        public float ReLU()
+        {
             float x = WeightedSum();
             return x > 0 ? x : 0;
         }
