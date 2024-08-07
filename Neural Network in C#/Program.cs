7f6226776be70d58fdf1304742e5ae7feac4d138
Jesus <jnoel883@gmail.com>
2024-08-06 18:46:46 -0600

    Valores de prueba de la red cambiados.

--- Neural Network in C#/Program.cs
+++ Neural Network in C#/Program.cs
@@ -7,11 +7,26 @@ E1  E2  AND
 1   1   1
 */
 
-// Perceptron perceptronSimple = new([1,0,0,1]);//inputs
-// perceptronSimple.AcitvationFunction("scalon");
-// Console.WriteLine(perceptronSimple.output);
-//inputs[]  , expectedOutput , learnRate?
-float[][] inputs =[[0,0],[0,1],[1,0],[1,1]];
-int[] espectedOutput=[0,0,0,1];
-Perceptron perceptronSimple = new(inputs[0].Length);
-perceptronSimple.Train(inputs, espectedOutput, 30);
+float[][] inputs = [[0,0],[0,1],[1,0],[1,1]]; // Datos de entrada
+float[] expectedOutput = [0,0,0,1]; // Salida esperada
+
+// Crear perceptrones para la capa oculta
+List<List<Perceptron>> hiddenLayers = new List<List<Perceptron>>
+{
+    new List<Perceptron> { new Perceptron(2, "sigmoid"), new Perceptron(2,"sigmoid") },
+    new List<Perceptron> { new Perceptron(2, "sigmoid"), new Perceptron(2, "sigmoid"), new Perceptron(2, "sigmoid"), new Perceptron(2,"sigmoid") },
+    new List<Perceptron> { new Perceptron(4, "sigmoid"), new Perceptron(4,"sigmoid"), new Perceptron(4, "sigmoid"), new Perceptron(4,"sigmoid") },
+};
+
+
+// Crear perceptrones para la capa de salida (1 perceptrón para la salida final)
+List<Perceptron> outputLayer = new List<Perceptron>
+{
+    new Perceptron(4, "step")
+};
+
+int epochs = 1000;
+
+// Inicializar el MLP
+MultiLayerPerceptron multiLayerPerceptron = new MultiLayerPerceptron(hiddenLayers, outputLayer, epochs);
+multiLayerPerceptron.Train(inputs, expectedOutput);
